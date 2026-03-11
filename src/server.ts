a053dfb3a7b2e6aea9c4c655fdd0a4fc48e5f3bb
Steven <muhammad95@dgu.ac.kr>
2026-03-12 08:24:35 +0900

    fix: modify server.ts

--- src/server.ts
+++ src/server.ts
@@ -8,12 +8,10 @@ mongoose.connect(process.env.MONGO_URL as string)
 .then((data) => {
     console.log("MondoDB connected Successfully")
     const PORT = process.env.PORT ?? 3001;
-// '0.0.0.0' qo'shildi - bu barcha tarmoq interfeyslarini tinglashni anglatadi
-app.listen(PORT as number, '0.0.0.0', function(){
-    console.info(`Server is running on: ${PORT}`);
-    // Endi bu yerda IP manzilingizni ko'rsatish foydaliroq
-    console.info(`Seller Page is running on: http://192.168.219.101:${PORT}/seller`);
-});
+    app.listen(PORT, function(){
+        console.info(`Server is running on: ${PORT}`);
+        console.info(`Seller Page is runnig on: http://localhost:${PORT}/seller `)
+    })
 }).catch((err) => {
     console.log("MongoDb connection failed", err);
 })
