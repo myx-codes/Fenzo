0af60dca96461404b6dc46772d836d07e220fdd8
Steven <muhammad95@dgu.ac.kr>
2026-03-17 02:53:37 +0900

    feat: deploy project via shell & pm2

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# PRODUCTION
+git reset --hard
+git checkout main
+git pull origin main
+
+npm i
+npm run build
+pm2 start process.config.js --env production
+
+# DEVELOPMENT
+# git reset --hard
+# git checkout develop
+# git pull origin develop
+
+# npm i
+# # pm2 start process.config.js --env development
+# pm2 start "run start:dev" --name=Fenzo 
+
+# chmod +x deploy.sh
+# -rwxr-xr-x    1 plass  staff     337 Mar 17 02:40 deploy.sh ==> READ, WRITE, EXECUTE 
