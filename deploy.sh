a93f62233c69e1fb584ce9d1ac11d488e6461cf6
Steven <muhammad95@dgu.ac.kr>
2026-03-18 12:00:51 +0900

    feat: deploy shell integration

--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,11 @@ git reset --hard
 git checkout main
 git pull origin main
 
-npm i
-npm run build
-pm2 start process.config.js --env production
+docker compose up -d
+
+# npm i
+# npm run build
+# pm2 start process.config.js --env production
 
 # DEVELOPMENT
 # git reset --hard
